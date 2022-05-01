7903d381ee5bc6604dbea3d4ba74fd9bcbd01384
Joscelyn Beatriz Guido de Henriquez <joscelyng21@gmail.com>
2022-04-30 20:10:44 -0600

    Add API call and style

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(__dirname + "/src"));
-//Configuraciones
+
 app.set("port", 3000);
-//routers
+
 app.get("/",(req, res) => {
     res.sendFile(path.join(__dirname, 'src/views/index.html'));
 
 });
 
 
-//static files
-//Listening the server
 app.listen(app.get('port'), () => {
 
     console.log("Server on port",app.get('port'));
